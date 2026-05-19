d65fa80992e402c83a1662bb76700db8f00ad533
angewonk <angelomallari1123@gmail.com>
2026-05-19 19:34:40 +0800

    updated vite config

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import {defineConfig} from 'vite';
 
 export default defineConfig(() => {
   return {
+    base: '/hapi-bortday-jusika/',
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
